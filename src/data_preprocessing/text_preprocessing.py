8c214c0dbb1f57c1e7cf7ef6a48ad51d6f1407e5
azywot <agata.zywot@gmail.com>
2023-11-11 09:17:16 +0100

    preprocess reviews text

---
+++ src/data_preprocessing/text_preprocessing.py
@@ -0,0 +1,72 @@
+import string
+
+from gensim.models.phrases import Phraser, Phrases
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from nltk.tokenize import word_tokenize
+
+
+def normalize_text(raw_text: str) -> list[str]:
+    """
+    Normalize a text.
+
+    :param raw_text: text to normalize
+    :return: list of normalized words
+    """
+    stop_words = set(stopwords.words("english"))
+    punctuation_table = str.maketrans({key: None for key in string.punctuation})
+    stemmer = SnowballStemmer("english")
+
+    try:
+        words = word_tokenize(raw_text)
+        sentence = []
+        for word in words:
+            try:
+                word = str(word)
+                lower_case_word = str.lower(word)
+                stemmed_word = stemmer.stem(lower_case_word)
+                no_punctuation = stemmed_word.translate(punctuation_table)
+
+                if len(no_punctuation) > 1 and no_punctuation not in stop_words:
+                    sentence.append(no_punctuation)
+            except:
+                continue
+
+        return sentence
+    except Exception as e:
+        return ""
+
+
+def normalize_sentences(sentences_tokenized: list[str]) -> list[str]:
+    """
+    Normalize a list of sentences.
+
+    :param sentences_tokenized: list of sentences to normalize
+    :return: list of normalized sentences
+    """
+    normalized_sentences = []
+    for s in sentences_tokenized:
+        normalized_text = normalize_text(s)
+        normalized_sentences.append(normalized_text)
+    return normalized_sentences
+
+
+def extract_phrases(
+    sentences_normalized: list[str], save_path: str = None
+) -> list[str]:
+    """
+    Extract phrases from a list of sentences.
+
+    :param sentences_normalized: list of normalized sentences
+    :param save: whether to save the model
+    :return: list of phrases
+    """
+    bigram_model = Phrases(sentences_normalized, min_count=100)
+    bigrams = [bigram_model[sent] for sent in sentences_normalized]
+    # NOTE: is it really tri-gram every time?
+    trigram_model = Phrases(bigrams, min_count=50)
+    phrased_sentences = [trigram_model[sent] for sent in bigrams]
+
+    if save_path is not None:
+        trigram_model.save(save_path + "food_trigrams.pkl")
+    return phrased_sentences
