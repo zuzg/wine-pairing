2c371488e86c40aae9bc560ac238b2154b889fda
azywot <agata.zywot@gmail.com>
2023-12-01 20:08:07 +0100

    add fg pairing

--- src/data_preprocessing/flavor_graph_preprocessing.py
+++ src/data_preprocessing/flavor_graph_preprocessing.py
@@ -1,4 +1,9 @@
+import pickle
+
+import numpy as np
 import pandas as pd
+
+from copy import deepcopy
 
 
 def create_wine_nodes(wine_items: list[str], max_id: int) -> pd.DataFrame:
@@ -60,3 +65,79 @@ def create_food_wine_edges(
 
     edges_df = pd.DataFrame(edges)
     return edges_df
+
+
+def load_embedding(file_name: str) -> dict:
+    """
+    Load embedding from pickle file.
+
+    :param file_name: path to pickle file
+    :return: dictionary of embeddings
+    """
+    with open(file_name, "rb") as handle:
+        embed_dict = pickle.load(handle)
+    return embed_dict
+
+
+def split_unanonimize_nodes(
+    nodes_df: pd.DataFrame, embed_dict: dict
+) -> dict[dict[np.array]]:
+    """
+    Split nodes into ingredient, compound and wine nodes.
+
+    :param nodes_df: nodes dataframe
+    :param embed_dict: dictionary of embeddings
+    :return: dictionary of embeddings split by node type
+    """
+    embed_dict_names = {}
+    for id, embed in embed_dict.items():
+        row = nodes_df[nodes_df["node_id"] == int(id)]
+        name = row["name"].values[0]
+        node_type = row["node_type"].values[0]
+
+        if node_type not in embed_dict_names:
+            embed_dict_names[node_type] = {}
+
+        embed_dict_names[node_type][name] = embed
+    return embed_dict_names
+
+
+def pair_item_with_category(
+    item: str,
+    fg_embed_dict: dict[dict[np.ndarray]],
+    pairing_category: str = "wine",
+    top_n: int = 1,
+):
+    """
+    Pair item with category.
+
+    :param item: item to pair
+    :param fg_embed_dict: dictionary of embeddings
+    :param pairing_category: category to pair with
+    :param top_n: number of items to return
+    :return: list of items
+    """
+    fg_embed_dict = deepcopy(fg_embed_dict)
+    ingredient_category = None
+    for key in fg_embed_dict.keys():
+        if item in fg_embed_dict[key].keys():
+            ingredient_category = key
+            break
+
+    if ingredient_category is None:
+        raise ValueError(
+            f"Item {item} not found in any category: {fg_embed_dict.keys()}"
+        )
+
+    item_embedding = fg_embed_dict[ingredient_category][item]
+    if ingredient_category == pairing_category:
+        del fg_embed_dict[ingredient_category][item]
+
+    pairing_category_embeddings = fg_embed_dict[pairing_category].values()
+    pairing_category_names = fg_embed_dict[pairing_category].keys()
+    pairing_category_embeddings = np.array(list(pairing_category_embeddings))
+    pairing_category_names = np.array(list(pairing_category_names))
+    distances = np.linalg.norm(item_embedding - pairing_category_embeddings, axis=1)
+
+    min_idxs = np.argsort(distances)[:top_n]
+    return pairing_category_names[min_idxs].tolist()
